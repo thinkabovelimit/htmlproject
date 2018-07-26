ce615782477b3c3da16b11313195d5a7aa1815a0
krishnaprasad <36506633+thinkabovelimit@users.noreply.github.com>
2018-07-26 10:22:24 +0530

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,31 @@
+// Restart Game Button
+var restart = document.querySelector('#b');
+
+// Grab all the squares
+var squares = document.querySelectorAll("td");
+
+
+// Clear Squares Function
+function clearboard() {
+  for (var i = 0; i < squares.length; i++) {
+      squares[i].textContent = '';
+  }
+
+}
+restart.addEventListener('click',clearboard)
+// Create a function that will check the square marker
+function changeMarker(){
+    if(this.textContent === ''){
+      this.textContent = 'X';
+      // console.log(marker)
+    }else if (this.textContent === 'X') {
+      this.textContent = 'O';
+    }else {
+      this.textContent = '';
+    }
+};
+
+// Use a for loop to add Event listeners to all the squares
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
+}
